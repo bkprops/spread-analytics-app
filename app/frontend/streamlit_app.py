860b85cafbb5e3ac87dacdc9ea26be57ada3d921
bkprops <balkbetting@gmail.com>
2025-11-04 11:20:27 +0000

    Update date input labels in Streamlit app for improved clarity by removing date format specification.

--- app/frontend/streamlit_app.py
+++ app/frontend/streamlit_app.py
@@ -314,11 +314,11 @@ def main() -> None:
         st.session_state[end_date_default_key] = default_end or ""
     col_start_date, col_end_date = st.sidebar.columns(2)
     col_start_date.text_input(
-        "Start date (YYYY-MM-DD)",
+        "Start date",
         key=start_date_key,
     )
     col_end_date.text_input(
-        "End date (YYYY-MM-DD)",
+        "End date",
         key=end_date_key,
     )
     st.sidebar.checkbox(
